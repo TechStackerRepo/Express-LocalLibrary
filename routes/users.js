a27f6bcd820a8b7ecae8660a17801323b6936d01
ibrahim.fofana <ibrahimfofana.techstacker@gmail.com>
2021-11-24 14:49:24 +0000

    challenge resolve

--- routes/users.js
+++ routes/users.js
@@ -6,14 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-// challenge routes
-// router.get('/cool', function(req, res, next) {
-//   res.send('you are so cool');
-// });
+ 
+ router.get('/cool', function(req, res, next) {
+    res.send('you are so cool');
+  });
 
-// router.get('/cool/nice', function(req, res, next) {
-//   res.send('you are so nice');
-// });
+  router.get('/cool/nice', function(req, res, next) {
+    res.send('you are so nice');
+ });
 
 
 

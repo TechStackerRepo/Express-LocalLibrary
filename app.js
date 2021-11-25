a08501747f82e7213e3b9c0d6bd2e429c1f07374
ibrahim.fofana <ibrahimfofana.techstacker@gmail.com>
2021-11-25 14:33:34 +0000

    connect locallibrary to mongoDB database

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://dbUser:root@cluster0.0wplo.mongodb.net/local_library?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -22,8 +30,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/users/cool', usersRouter);
- 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
